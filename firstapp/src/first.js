f74c4fe9c8824629b26d28ac86ed0d03643627f0
Pavan Kumar <kpavan36668@gmail.com>
2025-12-16 17:15:41 -0500

    updated firstapp

--- firstapp/src/first.js
+++ firstapp/src/first.js
@@ -3,16 +3,21 @@ import Second from "./second";
 import Sum from "./Sum";
 
 function Helloworld() {
-  const [state,setState]=useState({})
+  const [state, setState] = useState({
+    brand: "Ford",
+    model: "Mustang",
+    year: "1964",
+    color: "red",
+  });
+
   return (
-    
     <>
-
-
+      <>This is first component</>
+      <div>{state.brand}</div>
       <p>Hi helloo</p>
       <p>This is first component</p>
-      <Second/>
-      <Sum/>
+      <Second />
+      <Sum />
     </>
   );
 }
