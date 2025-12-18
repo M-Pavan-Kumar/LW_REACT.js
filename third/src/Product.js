ae8162511f95cb4b47bb39bc0a029c2c320236fe
Pavan Kumar <kpavan36668@gmail.com>
2025-12-18 23:45:52 +0530

    msg

--- third/src/Product.js
+++ third/src/Product.js
@@ -3,21 +3,19 @@ import { Link } from "react-router-dom";
 
 const Product = () => {
   const products = [
-    { id: 1, name: "Iphone", price: 100000 },
-    { id: 2, name: "Samsung", price: 30000 },
-    { id: 3, name: "Vivo", price: 350000 },
-    { id: 4, name: "redmi", price: 20000 },
+    { id: 1, name: "Iphone" },
+    { id: 2, name: "Samsung" },
+    { id: 3, name: "Vivo" },
+    { id: 4, name: "redmi" },
   ];
 
   return (
     <>
-      <h2>Product List</h2>
+      <div></div>
       {products.map((product) => (
-        <div key={product.id}>
-          <Link  to={`/product/${product.id}`}>
-            {product.name}
-          </Link>
-        </div>
+        <Link key={product.id} to={`/product/${product.id}`}>
+          {product.name}
+        </Link>
       ))}
     </>
   );
